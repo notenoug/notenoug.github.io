b0f30c7bc6ec8eb34918073d4f10cab0098555fe
notenoug <63596861+notenoug@users.noreply.github.com>
2020-04-20 18:49:40 +0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -8,21 +8,3 @@ arrows: false,
 pauseOnFocus: false,
 dots: true
 });
-
-
-	function initMap() {
-		var pos = {lat: 51.0465985, lng: 31.880841};
-		var opt = {
-			center: pos,
-			zoom: 18,
-		};
-		var map = new google.maps.Map(document.getElementById("map"), opt);
-
-		var marker = new google.maps.Marker({
-			position: pos,
-			map: map,
-			icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-			
-		});
-		
-	}
